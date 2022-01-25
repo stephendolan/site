ace9d55656aafe944a496c43b6e470f7f2751f36
Stephen Dolan <dolan.stephen1@gmail.com>
2022-01-25 11:38:38 -0500

    Try removing Turbo completely

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,13 +1,5 @@
 import "index.css";
-import * as Turbo from "@hotwired/turbo";
 import Alpine from "alpinejs";
-
-// Uncomment the line below to add transition animations when Turbo navigates.
-// We recommend adding <meta name="turbo-cache-control" content="no-preview" />
-// to your HTML head if you turn on transitions. Use data-turbo-transition="false"
-// on your <main> element for pages where you don't want any transition animation.
-//
-import "./turbo_transitions.js";
 
 // Optional, but can be useful for debugging.
 // window.Alpine = Alpine
