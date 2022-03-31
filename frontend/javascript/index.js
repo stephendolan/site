c115946958dd5db1abb595b7efb65dd83ec1c74c
Stephen Dolan <dolan.stephen1@gmail.com>
2022-03-31 14:00:31 -0400

    Add Fathom event attribution for SavvyCal bookings

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -8,3 +8,8 @@ Alpine.start();
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}";
+
+window.addEventListener("savvycal.scheduled", () => {
+  // Sessions are generally worth $500, unless someone books a 2-hour slot.
+  window.fathom.trackGoal("DBSMY0WK", 50000);
+});
