e5c9bb53b918bf99d4643e02c2f9a621bfb6e32c
Stephen Dolan <dolan.stephen1@gmail.com>
2023-10-08 13:51:21 -0400

    Move external scripts to JS

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,15 +1,21 @@
 import "$styles/index.css";
 import Alpine from "alpinejs";
 
-// Optional, but can be useful for debugging.
-// window.Alpine = Alpine
+import { loadSavvyCal, initSavvyCal, loadTwitter } from "./thirdPartyScripts";
+
+loadSavvyCal().then(() => {
+  initSavvyCal();
+
+  window.addEventListener("savvycal.scheduled", () => {
+    // Sessions are generally worth $500, unless someone books a 2-hour slot.
+    window.fathom.trackGoal("DBSMY0WK", 50000);
+  });
+});
+
+loadTwitter();
 
 Alpine.start();
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}";
 
-window.addEventListener("savvycal.scheduled", () => {
-  // Sessions are generally worth $500, unless someone books a 2-hour slot.
-  window.fathom.trackGoal("DBSMY0WK", 50000);
-});
---
+++ frontend/javascript/thirdPartyScripts.js
@@ -0,0 +1,33 @@
+// thirdPartyScripts.js
+export const loadSavvyCal = () => {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = "https://embed.savvycal.com/v1/embed.js";
+    script.async = true;
+    script.defer = true;
+    script.onload = () => {
+      resolve();
+    };
+    document.head.appendChild(script);
+  });
+};
+
+export const initSavvyCal = () => {
+  window.SavvyCal = window.SavvyCal || function () {
+    (window.SavvyCal.q = window.SavvyCal.q || []).push(arguments);
+  };
+  window.SavvyCal('init');
+};
+
+export const loadTwitter = () => {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+    script.defer = true;
+    script.onload = () => {
+      resolve();
+    };
+    document.head.appendChild(script);
+  });
+};
