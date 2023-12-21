b2484d269b80d557e242484911507109840bc5c1
Stephen Dolan <dolan.stephen1@gmail.com>
2023-12-21 10:36:51 -0500

    Switch analytics (again)

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -2,15 +2,13 @@ import "$styles/index.css";
 import Alpine from "alpinejs";
 import { Application } from "@hotwired/stimulus"
 
-import { loadHeapAnalytics } from "$javascript/thirdPartyScripts";
+import { loadAnalytics } from "$javascript/thirdPartyScripts";
 import twitterEmbed from "$javascript/alpine/twitterEmbed";
 import weatherController from "$javascript/stimulus/weather_controller.js";
 import countdownController from "$javascript/stimulus/countdown_controller.js";
 
-if (process.env.NODE_ENV !== "production") {
-  loadHeapAnalytics("1874465280");
-} else {
-  loadHeapAnalytics("1399095330");
+if (process.env.NODE_ENV === "production") {
+  loadAnalytics();
 }
 
 Alpine.data('twitterEmbed', twitterEmbed);
